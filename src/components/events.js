af343d28dc4d5c743a4001624fca252ebc7f186b
Brady Dowling <bradydowling@gmail.com>
2018-11-19 23:05:49 -0800

    Fix some React errors

--- src/components/events.js
+++ src/components/events.js
@@ -1,14 +1,34 @@
 import React from 'react'
 
-const Events = () => (
-    <div>
-        <h3>Upcoming activities</h3>
-        <ul>
-            <li>November 6, 7pm - Event title</li>
-            <li>November 13, 7pm - Event title2</li>
-            <li>November 20, 7pm - Event title2</li>
-        </ul>
-    </div>
-);
+import { getEvents } from './gcal'
+
+class Events extends React.Component {
+    constructor () {
+        super()
+        this.state = {
+          events: []
+        }
+    }
+
+    componentDidMount () {
+        getEvents((events) => {
+          this.setState({events})
+        })
+        console.log(this.state.events);
+    }
+
+    render() {
+        return (
+            <div>
+                <h3>Upcoming activities</h3>
+                <ul>
+                    <li>November 6, 7pm - Event title</li>
+                    <li>November 13, 7pm - Event title2</li>
+                    <li>November 20, 7pm - Event title2</li>
+                </ul>
+            </div>
+        )
+    }
+};
 
 export default Events;
