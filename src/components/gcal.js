4b85722a4aab674b0fe0a60d2ec825b99e88f5e8
Brady Dowling <bradydowling@gmail.com>
2018-11-19 22:59:31 -0800

    Start gcal integration

---
+++ src/components/gcal.js
@@ -0,0 +1,21 @@
+import request from 'superagent'
+
+let url = `https://www.googleapis.com/calendar/v3/calendars/${process.env.CAL_ID}/events?key=${process.env.GOOGLE_API_KEY}`
+
+export function getEvents (callback) {
+  request
+    .get(url)
+    .end((err, resp) => {
+      if (!err) {
+        const events = []
+        JSON.parse(resp.text).items.map((event) => {
+          events.push({
+            start: event.start.date || event.start.dateTime,
+            end: event.end.date || event.end.dateTime,
+            title: event.summary,
+          })
+        })
+        callback(events)
+      }
+    })
+}
